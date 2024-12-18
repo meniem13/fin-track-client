bec58ba311b6da3cd0a90eda311955464b9d4aa2
meniem nasr <125979500+meniem13@users.noreply.github.com>
2024-12-18 05:36:28 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -27,6 +27,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <meta name="description" content="meniem financial tracker" />
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
